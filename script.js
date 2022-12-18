9705e426a0ee1542c60ebee33e3eda5607082fd7
Andres Cardenas <65802352+DominicanCodes@users.noreply.github.com>
2022-12-18 12:38:40 -0500

    Add properly formatted grid Add hover action Add title

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-SIZE = 16
+SIZE = 16 * 16
 
 function displayGraph(size=SIZE) {
     const area = document.querySelector("#grid-container")
     for (i=0;i<size;i++) {
         const cell = document.createElement("div")
         cell.textContent = i + 1
+        // cell.id = i + 1
+        cell.classList = 'cell'
         area.appendChild(cell)
     }
 }
